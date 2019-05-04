96c5be923efd8f776536e881c47ff820bfa97892
Ben Spears <bspears@trinity.edu>
2019-05-04 14:49:39 -0500

    started db stuff

--- server/app/models/PetDBModel.scala
+++ server/app/models/PetDBModel.scala
@@ -1,5 +1,25 @@
 package models
 
+import javax.inject._
+import scala.collection.mutable.ArrayBuffer
+import slick.jdbc.MySQLProfile.api._
+import scala.concurrent.duration.Duration
+import scala.concurrent.Await
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
+import Tables._
+import com.mysql.cj.x.protobuf.MysqlxCrud.Limit
+
 class PetDBModel {
+  //def getList(username: String, db: Database)(implicit ec: ExecutionContext): Future[Seq[TasksRow]]
+  def getLogin(username: String, password: String, db: Database)(implicit ec: ExecutionContext): Future[Boolean] {
+    //TODO
+  }
+  
+  def getInfo(username: String, db: Database)implicit ec: ExecutionContext): Future[UserRow] {
+    //TODO
+  }
+  
+  
   
 }
