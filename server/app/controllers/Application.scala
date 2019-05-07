6f40fafffac77f4bfc0f4a69d852a6d8b7d80889
Blair Walker <bwalker4@trinity.edu>
2019-05-06 21:12:50 -0500

    merged login and basic views

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -1,16 +1,15 @@
 package controllers
 
 import javax.inject._
+import models._
+import play.api.data._
+import play.api.data.Forms._
 
 import edu.trinity.webapps.shared.SharedMessages
 import play.api.mvc._
 
 @Singleton
 class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-
-  def index = Action {
-    Ok(views.html.index(SharedMessages.itWorks))
-  }
 
   def map = Action {
     Ok(views.html.map())
@@ -29,6 +28,26 @@ class Application @Inject()(cc: ControllerComponents) extends AbstractController
   }
   
   def logout = Action { implicit request =>
-    Redirect(routes.Application.map).withNewSession
+    Redirect(routes.Application.login).withNewSession
+  }
+  
+  def index = Action {
+    Ok(views.html.index(SharedMessages.itWorks))
+  }
+  
+  def login = Action {
+    Ok(views.html.login())
+  }
+
+  def accountCreation = Action {
+    Ok(views.html.accountCreation())
+  }
+
+  def changePW = Action {
+    Ok(views.html.changePW())
+  }
+  
+  def chooseYourPet = Action {
+    Ok(views.html.chooseYourPet("Username"))
   }
 }
